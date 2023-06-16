b8f1614db2c8952e07099946a1b11695ed836871
shankar wangwad <shankarwangwad2018@gmail.com>
2023-06-16 17:50:18 +0530

    changed gear setting box

--- src/examples/Configurator/index.js
+++ src/examples/Configurator/index.js
@@ -1,17 +1,4 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 import { useState, useEffect } from "react";
 
@@ -286,7 +273,7 @@ function Configurator() {
         </MDBox>
         <Divider />
         <MDBox mt={3} mb={2}>
-          <MDButton
+          {/* <MDButton
             component={Link}
             href="https://www.creative-tim.com/learning-lab/react/quick-start/material-dashboard/"
             target="_blank"
@@ -296,10 +283,10 @@ function Configurator() {
             fullWidth
           >
             view documentation
-          </MDButton>
+          </MDButton> */}
         </MDBox>
         <MDBox display="flex" justifyContent="center">
-          <GitHubButton
+          {/* <GitHubButton
             href="https://github.com/creativetimofficial/material-dashboard-react"
             data-icon="octicon-star"
             data-size="large"
@@ -307,7 +294,7 @@ function Configurator() {
             aria-label="Star creativetimofficial/material-dashboard-react on GitHub"
           >
             Star
-          </GitHubButton>
+          </GitHubButton> */}
         </MDBox>
         <MDBox mt={2} textAlign="center">
           <MDBox mb={0.5}>
@@ -316,7 +303,7 @@ function Configurator() {
 
           <MDBox display="flex" justifyContent="center">
             <MDBox mr={1.5}>
-              <MDButton
+              {/* <MDButton
                 component={Link}
                 href="//twitter.com/intent/tweet?text=Check%20Material%20Dashboard%20React%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23react%20%mui&url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fmaterial-dashboard-react"
                 target="_blank"
@@ -325,9 +312,9 @@ function Configurator() {
               >
                 <TwitterIcon />
                 &nbsp; Tweet
-              </MDButton>
+              </MDButton> */}
             </MDBox>
-            <MDButton
+            {/* <MDButton
               component={Link}
               href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/material-dashboard-react"
               target="_blank"
@@ -336,7 +323,7 @@ function Configurator() {
             >
               <FacebookIcon />
               &nbsp; Share
-            </MDButton>
+            </MDButton> */}
           </MDBox>
         </MDBox>
       </MDBox>
